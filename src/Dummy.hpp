090909f31b4a7085ef3e0b13cfd110559657e375
Sylvain <sylvain.joyeux@tidewise.io>
2019-11-28 15:54:46 -0300

    Initial commit

---
+++ src/Dummy.hpp
@@ -0,0 +1,18 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+namespace nmea0183
+{
+    class DummyClass
+    {
+        public: 
+            /**
+             * Print a welcome to stdout
+             * \return nothing
+             */
+            void welcome();
+    };
+
+} // end namespace nmea0183
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
