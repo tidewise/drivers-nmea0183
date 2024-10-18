036ddf1d8a3d292e264b6e681324830ce1c75029
seixasxbr <mateus.seixas@tidewise.io>
2024-10-18 12:04:54 -0300

    chore: document

--- src/GPS.hpp
+++ src/GPS.hpp
@@ -8,18 +8,33 @@
 
 namespace nmea0183 {
     /**
+     * @brief Parses nmea 0183 messages into gps objects
      *
      */
     namespace GPS {
+        /**
+         * @brief Get the Solution object from the nmea 0183 rmc and gsa messages
+         *
+         * @param rmc The rmc message
+         * @param gsa The gsa message
+         * @return gps_base::Solution
+         */
         gps_base::Solution getSolution(marnav::nmea::rmc const& rmc,
             marnav::nmea::gsa const& gsa);
-
+        /**
+         * @brief Get the Solution Quality object from the nmea 0183 gsa message
+         *
+         * @param gsa The gsa message
+         * @return gps_base::SolutionQuality
+         */
         gps_base::SolutionQuality getSolutionQuality(marnav::nmea::gsa const& gsa);
-
-        gps_base::GPS_SOLUTION_TYPES getPositionType(marnav::nmea::mode_indicator);
-        base::Time buildRockTime(
-            marnav::utils::optional<marnav::nmea::time> const& optional_time,
-            marnav::utils::optional<marnav::nmea::date> const& optional_date);
+        /**
+         * @brief Get the Position Type object from the nmea 0183 mode indicator
+         *
+         * @param mode The mode indicator
+         * @return gps_base::GPS_SOLUTION_TYPES
+         */
+        gps_base::GPS_SOLUTION_TYPES getPositionType(marnav::nmea::mode_indicator mode);
     };
 }
 
