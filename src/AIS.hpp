4e41083d5c9dabfbbcf7f686dc97ae7689ff0ba6
eduardacoppo <eduarda.coppo@tidewise.io>
2024-11-14 15:17:42 -0300

    chore: format code

--- src/AIS.hpp
+++ src/AIS.hpp
@@ -1,11 +1,11 @@
 #ifndef NMEA0183_AIS_HPP
 #define NMEA0183_AIS_HPP
 
-#include <marnav/ais/message.hpp>
-#include <nmea0183/Driver.hpp>
 #include <ais_base/Position.hpp>
 #include <ais_base/VesselInformation.hpp>
 #include <ais_base/VoyageInformation.hpp>
+#include <marnav/ais/message.hpp>
+#include <nmea0183/Driver.hpp>
 
 #include <marnav/ais/message_01.hpp>
 #include <marnav/ais/message_05.hpp>
@@ -37,16 +37,14 @@ namespace nmea0183 {
          * The returned value will either be NULL or point to an AIS message
          */
         std::unique_ptr<marnav::ais::message> processSentence(
-            marnav::nmea::sentence const& sentence
-        );
+            marnav::nmea::sentence const& sentence);
 
         /** Returns the count of sentences that have been discarded because
          * of some reordering/reassembly issues
          */
         uint32_t getDiscardedSentenceCount() const;
 
-        static ais_base::Position getPosition(
-            marnav::ais::message_01 const& message);
+        static ais_base::Position getPosition(marnav::ais::message_01 const& message);
         static ais_base::VesselInformation getVesselInformation(
             marnav::ais::message_05 const& message);
         static ais_base::VoyageInformation getVoyageInformation(
